669159b3d404b74e2804d288fa06e60285a17878
ManojAvs <ManojAvs@Lenovo-PC>
2020-07-05 20:12:58 +0530

    my second commit

--- GitTestViaEclipse/src/com/test/ecli/Class2.java
+++ GitTestViaEclipse/src/com/test/ecli/Class2.java
@@ -2,7 +2,8 @@ package com.test.ecli;
 
 public class Class2 {
 	public static void main(String[] args) {
-		System.out.println("Class2 " + System.currentTimeMillis());
+		System.out.println("Class2 line1 " + System.currentTimeMillis());
+		System.out.println("Class2 line2" + System.currentTimeMillis());
 	}
 
 }
