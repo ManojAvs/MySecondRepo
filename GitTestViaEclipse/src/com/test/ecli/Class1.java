395658f04ec384a0b9354cfbd6fdcad902266683
ManojAvs <67865298+ManojAvs@users.noreply.github.com>
2020-07-05 20:07:06 +0530

    Update Class1.java

--- GitTestViaEclipse/src/com/test/ecli/Class1.java
+++ GitTestViaEclipse/src/com/test/ecli/Class1.java
@@ -6,6 +6,7 @@ public class Class1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Test 1"+Calendar.getInstance().getTime());
+		System.out.println("Test 2"+Calendar.getInstance().getTime());
 	}
 
 }
