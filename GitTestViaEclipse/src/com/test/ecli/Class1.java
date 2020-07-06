71940fed87a62208703e3ea04061cd0053eac380
ManojAvs <ManojAvs@Lenovo-PC>
2020-07-06 13:30:59 +0530

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/ManojAvs/MySecondRepo.git
    
    Conflicts:
    	GitTestViaEclipse/src/com/test/ecli/Class1.java

--- GitTestViaEclipse/src/com/test/ecli/Class1.java
+++ GitTestViaEclipse/src/com/test/ecli/Class1.java
@@ -4,14 +4,11 @@ import java.util.Calendar;
 
 public class Class1 {
 	public static void main(String[] args) {
-		
-		System.out.println("Test 1"+Calendar.getInstance().getTime());
-		System.out.println("Test 2"+Calendar.getInstance().getTime());
-		
-		
-		
-		System.out.println("Line 3"+Calendar.getInstance().getTime());
-		
+
+		System.out.println("Test 1" + Calendar.getInstance().getTime());
+		System.out.println("Test 2" + Calendar.getInstance().getTime());
+		System.out.println("Line 3" + Calendar.getInstance().getTime());
+
 	}
 
 }
