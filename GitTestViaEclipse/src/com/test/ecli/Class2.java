82564f6d786d977d1d502a0c26042f2a1407d4b2
ManojAvs <67865298+ManojAvs@users.noreply.github.com>
2020-07-06 12:55:48 +0530

    Update Class2.java

--- GitTestViaEclipse/src/com/test/ecli/Class2.java
+++ GitTestViaEclipse/src/com/test/ecli/Class2.java
@@ -8,6 +8,7 @@ public class Class2 {
 		System.out.println("Class2 line4 -- modified viaGitHub" + System.currentTimeMillis());
 		System.out.println("Class2 line5 -- Newly added via Eclipse" + System.currentTimeMillis());
 		System.out.println("Class2 line6 -- Newly Modified Via Eclipse" + System.currentTimeMillis());
+		System.out.println("Class2 line7 -- Added via GitHub" + System.currentTimeMillis());
 
 	}
 
